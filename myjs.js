95525f20dd0395371f8c53a3f8aa915a300c56af
MINSU1 <31994561+MINSU1@users.noreply.github.com>
2017-10-19 21:56:53 -0700

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -1,23 +1,77 @@
-document.getElementById("open").addEventListener("click", function(){
-                                                 document.getElementById("control").style.left = "0px";
-                                                 });
+var chatStr = "";
 
-document.getElementById("bg1").addEventListener("click", function(){
-    document.getElementById("backgroundImage").style.backgroundImage = "url(img/f1.jpg)";
+function replaceEmojis(){
+    chatStr = chatStr.replace(":)", "<img class='emoji' src='image/smile.png' />");
+    chatStr = chatStr.replace(":(", "<img class='emoji' src='image/angry.png' />"); 
+    chatStr = chatStr.replace(":'(", "<img class='emoji' src='image/cry.png' />");
+    chatStr = chatStr.replace(";)", "<img class='emoji' src='image/kappa.png' />");
+    chatStr = chatStr.replace(":...)", "<img class='emoji' src='image/think.png' />");
+}
+
+function censor(){
+    chatStr = chatStr.replace("boogie", "******");
+    chatStr = chatStr.replace("boobie", "******");
+    chatStr = chatStr.replace("soogie", "******");
+
+}
+
+
+/*--------------------------p1Chat--------------------------*/
+function changeChatStr(){
+    chatStr = document.getElementById("p1Input").value;
+    document.getElementById("p1Input").value = "";
+    
+    /*if(chatStr == ":)") {
+        chatStr = "<img class='emoji' src='image/smile.png' />";
+    } else if(chatStr == ":(") {
+        chatStr = "<img class='emoji' src='image/sad.png' />";
+    }*/
+    
+    /*chatStr = chatStr.replace(":)", "<img class='emoji' src='image/smile.png' />");
+    chatStr = chatStr.replace(":(", "<img class='emoji' src='image/sad.png' />");*/
+    replaceEmojis()
+    censor()
+}
+
+/*--------------------------p2Chat--------------------------*/
+function changeChatStr2(){
+    chatStr = document.getElementById("p2Input").value;
+    document.getElementById("p2Input").value = "";
+    
+    /*chatStr = chatStr.replace(":)", "<img class='emoji' src='image/smile.png' />");
+    chatStr = chatStr.replace(":(", "<img class='emoji' src='image/sad.png' />");*/
+    replaceEmojis()
+    censor()
+}
+
+/*--------------------------chatDisplay--------------------------*/
+function createChat(chatNum){
+    var ndiv = document.createElement("div");
+    ndiv.innerHTML = chatStr;
+    
+    if(chatNum == 1){
+        ndiv.style.backgroundColor = "#AFA";
+    } else if (chatNum == 2){
+        ndiv.style.backgroundColor = "#AAF";
+    }
+    
+    ndiv.style.padding = "10px";
+    
+    
+    document.getElementById("chatDisplay").appendChild(ndiv);
+}
+
+/*--------------------------Interactions--------------------------*/
+document.getElementById("p1Input").addEventListener("keyup", function(ev){
+    if(ev.keyCode == 13){
+        changeChatStr();
+        createChat(1);
+    }
 });
 
-document.getElementById("bg2").addEventListener("click", function(){
-    document.getElementById("backgroundImage").style.backgroundImage = "url(img/f2.jpg)";
+document.getElementById("p2Input").addEventListener("keyup", function(ev){
+    if(ev.keyCode == 13){
+        changeChatStr2();
+        createChat(2);
+    }
 });
-
-document.getElementById("bg3").addEventListener("click", function(){
-    document.getElementById("backgroundImage").style.backgroundImage = "url(img/f3.jpg)";
-});
-
-document.getElementById("bg4").addEventListener("click", function(){
-    document.getElementById("backgroundImage").style.backgroundImage = "url(img/f111.png)";
-});
-
-document.getElementById("close").addEventListener("click", function(){
-                                                 document.getElementById("control").style.left = "-200px";
-                                                 });
