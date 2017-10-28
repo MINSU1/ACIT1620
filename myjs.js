7578e767f2994e930eadb09d7ad347b1ba1c9e72
MINSU1 <31994561+MINSU1@users.noreply.github.com>
2017-10-28 01:55:46 -0700

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -1,77 +1,96 @@
-var chatStr = "";
-
-function replaceEmojis(){
-    chatStr = chatStr.replace(":)", "<img class='emoji' src='image/smile.png' />");
-    chatStr = chatStr.replace(":(", "<img class='emoji' src='image/angry.png' />"); 
-    chatStr = chatStr.replace(":'(", "<img class='emoji' src='image/cry.png' />");
-    chatStr = chatStr.replace(";)", "<img class='emoji' src='image/kappa.png' />");
-    chatStr = chatStr.replace(":...)", "<img class='emoji' src='image/think.png' />");
-}
-
-function censor(){
-    chatStr = chatStr.replace("boogie", "******");
-    chatStr = chatStr.replace("boobie", "******");
-    chatStr = chatStr.replace("soogie", "******");
-
-}
-
-
-/*--------------------------p1Chat--------------------------*/
-function changeChatStr(){
-    chatStr = document.getElementById("p1Input").value;
-    document.getElementById("p1Input").value = "";
-    
-    /*if(chatStr == ":)") {
-        chatStr = "<img class='emoji' src='image/smile.png' />";
-    } else if(chatStr == ":(") {
-        chatStr = "<img class='emoji' src='image/sad.png' />";
-    }*/
-    
-    /*chatStr = chatStr.replace(":)", "<img class='emoji' src='image/smile.png' />");
-    chatStr = chatStr.replace(":(", "<img class='emoji' src='image/sad.png' />");*/
-    replaceEmojis()
-    censor()
-}
-
-/*--------------------------p2Chat--------------------------*/
-function changeChatStr2(){
-    chatStr = document.getElementById("p2Input").value;
-    document.getElementById("p2Input").value = "";
-    
-    /*chatStr = chatStr.replace(":)", "<img class='emoji' src='image/smile.png' />");
-    chatStr = chatStr.replace(":(", "<img class='emoji' src='image/sad.png' />");*/
-    replaceEmojis()
-    censor()
-}
-
-/*--------------------------chatDisplay--------------------------*/
-function createChat(chatNum){
-    var ndiv = document.createElement("div");
-    ndiv.innerHTML = chatStr;
-    
-    if(chatNum == 1){
-        ndiv.style.backgroundColor = "#AFA";
-    } else if (chatNum == 2){
-        ndiv.style.backgroundColor = "#AAF";
+var menudisplay=1;
+var theTop=0;
+var theLeft=0;
+var Bgsize=300;
+//---------------------Functions---------------------------
+function expandMenu(){
+    if(menudisplay==1){
+        document.getElementById("controls").style.bottom="10px";
+        menudisplay = 0;
+    }
+    else if(menudisplay==0){
+        document.getElementById("controls").style.bottom="-100px";
+        menudisplay = 1;
     }
     
-    ndiv.style.padding = "10px";
+};
+
+function changeColor(theValue){
+    document.getElementById("title").style.color = theValue;
+    document.getElementById("description").style.color = theValue;
+};
+
+function changeDescription(theValue){
+        document.getElementById("description").innerHTML = theValue;
+};
+
+function changeTitle(theValue){
+    document.getElementById("title").innerHTML = theValue;
+};
+
+function changeBG(theName){
+    if(theName.indexOf("http") != -1){
+        document.getElementById("background").style.backgroundImage= "url("+theName+")";
+    }else if(theName == "horse"){
+        document.getElementById("background").style.backgroundImage ="url(img/bg1.jpg)";
+    }else if(theName == "night"){
+        document.getElementById("background").style.backgroundImage ="url(img/bg2.jpg)";
+    }else if(theName == "mountain"){
+        document.getElementById("background").style.backgroundImage ="url(img/bg3.jpg)";
+    }else if(theName.indexOf("epic") != -1){
+        document.getElementById("background").style.backgroundImage ="url(img/bg4.jpg)";
+    }
     
-    
-    document.getElementById("chatDisplay").appendChild(ndiv);
 }
 
-/*--------------------------Interactions--------------------------*/
-document.getElementById("p1Input").addEventListener("keyup", function(ev){
+function moveBG(keycode){
+    if(keycode==37){
+        theLeft = theLeft - 10;
+    };
+     if(keycode==39){
+        theLeft = theLeft + 10;
+    };
+     if(keycode==38){
+        theTop = theTop - 10;
+    };
+     if(keycode==40){
+        theTop = theTop + 10;
+     };
+     if(keycode == 189){
+        Bgsize = Bgsize - 10;
+    };
+     if(keycode == 187){
+        Bgsize = Bgsize + 10;
+    };
+    document.getElementById("background").style.backgroundPosition=theLeft+"px"+" "+theTop+"px";
+    document.getElementById("background").style.height=Bgsize+"px";
+};
+//---------------------------------------------------------
+
+document.getElementById("menu").addEventListener("click", function(){
+    expandMenu();
+});
+
+document.body.addEventListener("keydown",function(ev){
+    moveBG(ev.keyCode);
+});
+
+document.getElementById("fileIn").addEventListener("keydown",function(ev){
     if(ev.keyCode == 13){
-        changeChatStr();
-        createChat(1);
+        changeBG(fileIn.value)
     }
 });
 
-document.getElementById("p2Input").addEventListener("keyup", function(ev){
-    if(ev.keyCode == 13){
-        changeChatStr2();
-        createChat(2);
-    }
+document.getElementById("desIn").addEventListener("keydown",function(){
+    var thedesValue = document.getElementById("desIn").value;
+    changeDescription(thedesValue);
+});
+
+document.getElementById("titleIn").addEventListener("keydown",function(){
+    var thetitleValue = document.getElementById("titleIn").value;
+    changeTitle(thetitleValue);
+});
+
+document.getElementById("colorbar").addEventListener("change",function(){
+    changeColor(colorbar.value)
 });
